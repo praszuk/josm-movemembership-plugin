b13f779a589a6f15c41a95313e8075a0f2ce0668
Paweł Raszuk <pawel.raszuk@gmail.com>
2023-02-01 15:14:15 +0100

    Init plugin project files

---
+++ src/main/java/org/openstreetmap/josm/plugins/movemembership/MoveMembershipPlugin.java
@@ -0,0 +1,10 @@
+package org.openstreetmap.josm.plugins.movemembership;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+public class MoveMembershipPlugin extends Plugin {
+    public MoveMembershipPlugin(PluginInformation info) {
+        super(info);
+    }
+}
